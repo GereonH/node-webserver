9371fe4b63163e27c79e91c7aad2c32052d35b41
Heinemann, Gereon <gereon.heinemann@stud.hs-ruhrwest.de>
2018-12-02 16:50:18 +0100

    Initial

---
+++ server.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const hbs     = require('hbs');
+const fs      = require('fs')
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs');
+
+app.use((req,res,next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log('unable to append to server.log');
+    }
+  })
+  next(); 
+})
+
+// app.use((req,res,next) => {
+//   res.render('maintenance.hbs')
+// })
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+})
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+})
+
+app.get('/',(req, res) => {
+  res.render('home.hbs', {
+    pageTitel: 'Homepage',
+    welcomeMsg: 'Hallo du Lappen'
+  })
+})
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitel: 'About Page',
+  })
+})
+
+app.get('/bad',(req, res) => {
+  // res.send('Hello');
+  res.send({
+    errorMessage: 'Error aufgetreten'
+  })
+})
+
+app.listen(3000, () => {
+  console.log('Server is up on Port 3000');
+});
