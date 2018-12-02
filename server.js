a5329cddb9ee071eaabeace4a4c7c15e73d234d0
Heinemann, Gereon <gereon.heinemann@stud.hs-ruhrwest.de>
2018-12-02 17:06:09 +0100

    heroku ready

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const hbs     = require('hbs');
 const fs      = require('fs')
 
+const port = process.env.PORT || 3000;
+
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials')
@@ -16,7 +18,7 @@ app.use((req,res,next) => {
       console.log('unable to append to server.log');
     }
   })
-  next(); 
+  next();
 })
 
 // app.use((req,res,next) => {
@@ -52,6 +54,6 @@ app.get('/bad',(req, res) => {
   })
 })
 
-app.listen(3000, () => {
-  console.log('Server is up on Port 3000');
+app.listen(port, () => {
+  console.log(`Server is up on Port ${port}`);
 });
